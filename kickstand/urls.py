bc3bbf6d3458e5dc4493916734f4480001b1bd11
peter <peter@sportsdraftdaily.com>
2014-03-18 14:05:00 -0400

    add jqm code

--- kickstand/urls.py
+++ kickstand/urls.py
@@ -3,10 +3,29 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from kickstand.views import HomePageView
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'kickstand.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    #url(r'^$', HomePageView.as_view(), name='home'),
+    url(r'^/?$', 'jqm.views.home', name='home'),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(
+        r'^accounts/login/$','django.contrib.auth.views.login',
+        dict(
+            template_name = 'jqm/login.html',
+        ),
+        name='login',
+    ),
+    url(
+        r'^accounts/logout/$','django.contrib.auth.views.logout',
+        dict(
+            template_name = 'jqm/logout.html',
+        ),
+        name='logout',
+    ),
+
 )
