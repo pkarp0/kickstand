11210d365b9d09352ca22bc20fec9dd9409335a0
peter <peter@sportsdraftdaily.com>
2014-03-29 12:59:42 -0400

    rm gistest

--- gistest/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib.gis import admin
-from gistest.models import Place
-
-admin.site.register(Place, admin.OSMGeoAdmin)
--- gistest/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from gistest.models import Place
-
-class PlaceForm(ModelForm):
-    class Meta:
-        model = Place
-        fields = ['name', 'address', 'coord']
